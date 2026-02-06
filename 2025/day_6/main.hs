38268a93824c876de3590b4b35aefad477c465ed
Frigyes Kiss <kssfrigyes@gmail.com>
2026-02-06 12:37:26 +0100

    [fix] 2025 day 6

--- 2025/day_6/main.hs
+++ 2025/day_6/main.hs
@@ -9,15 +9,14 @@ splitOn d xs = chunk : splitOn d (drop 1 rest)
   where (chunk, rest) = span (/=d) xs
 
 partOne :: String -> Int
-partOne str = sum $ (parseCol . reverse) <$> transpose (words <$> lines str)
+partOne str = sum $ parseCol . reverse <$> transpose (words <$> lines str)
   where parseCol (o:ns) = foldr1 (op o) (read <$> ns)
 
 partTwo :: String -> Int
-partTwo str = sum $ zipWith fold (map op ops) nss
+partTwo str = sum $ zipWith foldr1 (map op ops) nss
   where ops = words $ last (lines str)
         cols = splitOn "    " (init <$> transpose (lines str))
         nss = (map . map) (read . concat . words) cols
-        fold o c = foldr1 o c
 
 main :: IO ()
 main = do
